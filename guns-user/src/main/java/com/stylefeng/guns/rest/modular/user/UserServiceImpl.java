730ef21f424a0d8effe88ff35f15ae6b0ea6105b
utf-24 <13051176282@163.com>
2018-11-04 01:01:48 +0800

    4-15用户名检查

--- guns-user/src/main/java/com/stylefeng/guns/rest/modular/user/UserServiceImpl.java
+++ guns-user/src/main/java/com/stylefeng/guns/rest/modular/user/UserServiceImpl.java
@@ -1,6 +1,7 @@
 package com.stylefeng.guns.rest.modular.user;
 
 import com.alibaba.dubbo.config.annotation.Service;
+import com.baomidou.mybatisplus.mapper.EntityWrapper;
 import com.stylefeng.guns.api.user.UserApi;
 import com.stylefeng.guns.api.user.vo.UserInfoModel;
 import com.stylefeng.guns.api.user.vo.UserModel;
@@ -8,7 +9,6 @@ import com.stylefeng.guns.core.util.MD5Util;
 import com.stylefeng.guns.rest.common.persistence.dao.MoocUserTMapper;
 import com.stylefeng.guns.rest.common.persistence.model.MoocUserT;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.AutoConfigureOrder;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -20,7 +20,17 @@ public class UserServiceImpl implements UserApi {
 
     @Override
     public int login(String username, String password) {
+        MoocUserT moocUserT = new MoocUserT();
+        moocUserT.setUserName(username);
+        //根据用户名查找数据库用户信息
+        MoocUserT result =  moocUserTMapper.selectOne(moocUserT);
 
+        if(null!=result && result.getUuid()>0){
+            String md5PassWord = MD5Util.encrypt(password);
+            if(result.getUserPwd().equals(md5PassWord)){
+                return result.getUuid();
+            }
+        }
         return 0;
     }
 
@@ -49,16 +59,72 @@ public class UserServiceImpl implements UserApi {
 
     @Override
     public boolean checkUsername(String username) {
-        return false;
+        EntityWrapper<MoocUserT> entityWrapper = new EntityWrapper<>();
+        entityWrapper.eq("user_name",username);
+        Integer result = moocUserTMapper.selectCount(entityWrapper);
+
+        if(null!=result && result>0){
+            return true;
+        }else {
+            return false;
+        }
+    }
+
+    private UserInfoModel do2UserInfo(MoocUserT moocUserT){
+        UserInfoModel userInfoModel = new UserInfoModel();
+
+        userInfoModel.setUuid(moocUserT.getUuid());
+        userInfoModel.setHeadAddress(moocUserT.getHeadUrl());
+        userInfoModel.setPhone(moocUserT.getUserPhone());
+        userInfoModel.setUpdateTime(moocUserT.getUpdateTime().getTime());
+        userInfoModel.setEmail(moocUserT.getEmail());
+        userInfoModel.setUsername(moocUserT.getUserName());
+        userInfoModel.setNickname(moocUserT.getNickName());
+        userInfoModel.setLifeState(""+moocUserT.getLifeState());
+        userInfoModel.setBirthday(moocUserT.getBirthday());
+        userInfoModel.setAddress(moocUserT.getAddress());
+        userInfoModel.setSex(moocUserT.getUserSex());
+        userInfoModel.setBeginTime(moocUserT.getBeginTime().getTime());
+        userInfoModel.setBiography(moocUserT.getBiography());
+
+        return userInfoModel;
     }
 
     @Override
     public UserInfoModel getUserInfo(int uuid) {
-        return null;
+        //是否应该考虑id未查出来的情况
+        MoocUserT moocUserT = moocUserTMapper.selectById(uuid);
+        //将moocUserT数据转换为UserinfoModel
+        UserInfoModel userInfoModel = do2UserInfo(moocUserT);
+        return userInfoModel;
     }
 
     @Override
     public UserInfoModel updateUserInfo(UserInfoModel userInfoModel) {
-        return null;
+        MoocUserT moocUserT = new MoocUserT();
+
+        moocUserT.setUuid(userInfoModel.getUuid());
+        moocUserT.setNickName(userInfoModel.getNickname());
+        moocUserT.setLifeState(Integer.parseInt(userInfoModel.getLifeState()));
+        moocUserT.setBirthday(userInfoModel.getBirthday());
+        moocUserT.setBiography(userInfoModel.getBiography());
+        moocUserT.setBeginTime(null);
+        moocUserT.setHeadUrl(userInfoModel.getHeadAddress());
+        moocUserT.setEmail(userInfoModel.getEmail());
+        moocUserT.setAddress(userInfoModel.getAddress());
+        moocUserT.setUserPhone(userInfoModel.getPhone());
+        moocUserT.setUserSex(userInfoModel.getSex());
+        moocUserT.setUpdateTime(null);
+
+        //Do存入数据库
+        Integer result = moocUserTMapper.updateById(moocUserT);
+        if(result>0){
+            //再从数据库查出来
+            UserInfoModel userInfo = getUserInfo(moocUserT.getUuid());
+            //结果返回前端
+            return userInfo;
+        }else{
+            return null;
+        }
     }
 }
