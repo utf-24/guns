64aacb3f575d842d9849d09cb3bedfff0148bcce
utf-24 <13051176282@163.com>
2018-11-15 00:57:02 +0800

    5-16 getSources,cat,year

---
+++ guns-film/src/main/java/com/stylefeng/guns/rest/common/persistence/dao/MoocActorTMapper.java
@@ -0,0 +1,16 @@
+package com.stylefeng.guns.rest.common.persistence.dao;
+
+import com.stylefeng.guns.rest.common.persistence.model.MoocActorT;
+import com.baomidou.mybatisplus.mapper.BaseMapper;
+
+/**
+ * <p>
+ * 演员表 Mapper 接口
+ * </p>
+ *
+ * @author young
+ * @since 2018-11-11
+ */
+public interface MoocActorTMapper extends BaseMapper<MoocActorT> {
+
+}
