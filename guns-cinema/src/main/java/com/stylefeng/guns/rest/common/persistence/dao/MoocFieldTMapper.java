dd09e3c01d20ebe518c75c3576c4dc498237552a
utf-24 <13051176282@163.com>
2018-11-25 17:21:51 +0800

    6-10 cinema/getCinemas

---
+++ guns-cinema/src/main/java/com/stylefeng/guns/rest/common/persistence/dao/MoocFieldTMapper.java
@@ -0,0 +1,16 @@
+package com.stylefeng.guns.rest.common.persistence.dao;
+
+import com.stylefeng.guns.rest.common.persistence.model.MoocFieldT;
+import com.baomidou.mybatisplus.mapper.BaseMapper;
+
+/**
+ * <p>
+ * 放映场次表 Mapper 接口
+ * </p>
+ *
+ * @author young
+ * @since 2018-11-25
+ */
+public interface MoocFieldTMapper extends BaseMapper<MoocFieldT> {
+
+}
